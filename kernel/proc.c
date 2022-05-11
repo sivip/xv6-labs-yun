2ed8c4eb7266f17d228671879dd7de7a5d4c4c67
赵云 <58766762@qq.com>
2022-05-11 21:02:07 +0800

    util lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -140,6 +140,9 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p->syscall_trace = 0; // (newly added) 为 syscall_trace 设置一个 0 的默认值
+
+
 
   return p;
 }
@@ -302,6 +305,8 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+
+  np->syscall_trace = p->syscall_trace; // HERE!!! 子进程继承父进程的 syscall_trace
 
   pid = np->pid;
 
